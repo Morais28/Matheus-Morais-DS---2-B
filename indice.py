24d9f383361ad9a7ae0d6ca818cc90c437283ba7
Morais28 <morais28102008@gmail.com>
2025-10-03 13:32:06 -0300

    Add files via upload

---
+++ indice.py
@@ -0,0 +1,5 @@
+comidas = ["pizza", "sushi", "hamburguer", "açaí", "lasanha", "hot-dog", "churrasco"]
+
+indice = int(input("digite um número de 0 a 6:"))
+
+print("voçê escolheu: ", comidas[indice])
---
+++ vetor.py
@@ -0,0 +1,11 @@
+# cores = ["vermelho", "roxo", "azul", "verde", "amarelo"]
+
+# print("As cores são", cores)
+
+frutas = ["maçã", "pera", "uva", "pessego", "banana"]
+
+print("Primeira fruta: ", frutas[0])
+print("ultima fruta: ", frutas[4])
+
+frutas[2] = "morango"
+print("nova vetor: ", frutas)
